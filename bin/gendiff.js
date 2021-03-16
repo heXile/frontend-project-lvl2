caa9c83aa19f74fab364dd4b423e2ce72ecc4760
Antoni Mcyr <xiberian@icloud.com>
2021-03-16 10:40:58 +0300

    merging changes from another machine

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 
-// const program = new Command();
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
