f52d6cfbc67738166f25b917531b26c96e3dc2d2
Antoni Mcyr <xiberian@icloud.com>
2021-05-20 17:01:07 +0300

    Added fixtures and updated tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,21 @@
 import genDiff from '../src/index.js';
 import { readFileContent } from '../src/utils.js';
 
-const result = readFileContent('__fixtures__/result1');
-
-test('genDiff', () => {
+test('gendiff main functionality', () => {
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(
-    result
+    readFileContent('__fixtures__/result1to2')
   );
+  expect(genDiff('__fixtures__/file2.json', '__fixtures__/file1.json')).toEqual(
+    readFileContent('__fixtures__/result2to1')
+  );
+});
+
+test('Comparing correct to an empty json file', () => {
+  expect(genDiff('__fixtures__/file2.json', '__fixtures__/empty.json')).toEqual(
+    readFileContent('__fixtures__/result2toE')
+  );
+});
+
+test('Comparing 2 empty json files', () => {
+  expect(genDiff('__fixtures__/empty.json', '__fixtures__/empty.json')).toEqual('{\n}');
 });
