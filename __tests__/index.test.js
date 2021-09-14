de7e6d69a0068e0e4f6f5ed644007b09465628a3
Antoni Mcyr <xiberian@icloud.com>
2021-09-14 20:31:46 +0300

    Tests modified with describe.eash method

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,53 +6,37 @@ import { readFileContent } from '../src/utils.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) =>
-  path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFormatDescription = (format) => (format === 'plain' ? 'plain text' : format);
 
-test('gendiff main json functionality', () => {
-  expect(
-    genDiff(
-      getFixturePath('fileLeft.json'),
-      getFixturePath('fileRight.json'),
-      'stylish'
-    )
-  ).toEqual(readFileContent(getFixturePath('result-stylish.txt')));
-  expect(
-    genDiff(
-      getFixturePath('fileLeft.json'),
-      getFixturePath('fileRight.json'),
-      'plain'
-    )
-  ).toEqual(readFileContent(getFixturePath('result-plain.txt')));
-  expect(
-    genDiff(
-      getFixturePath('fileLeft.json'),
-      getFixturePath('fileRight.json'),
-      'json'
-    )
-  ).toEqual(readFileContent(getFixturePath('result.json')));
+describe.each([
+  ['fileLeft.json', 'fileRight.json', 'stylish', 'result-stylish.txt'],
+  ['fileLeft.json', 'fileRight.json', 'plain', 'result-plain.txt'],
+  ['fileLeft.json', 'fileRight.json', 'json', 'result.json'],
+])('JSON files comparison:', (fileLeft, fileRight, outputFormat, expected) => {
+  test(`${getFormatDescription(outputFormat)} output: `, () => {
+    expect(
+      genDiff(
+        getFixturePath(fileLeft),
+        getFixturePath(fileRight),
+        outputFormat,
+      ),
+    ).toEqual(readFileContent(getFixturePath(expected)));
+  });
 });
 
-test('gendiff main yaml functionality', () => {
-  expect(
-    genDiff(
-      getFixturePath('fileLeft.yml'),
-      getFixturePath('fileRight.yml'),
-      'stylish'
-    )
-  ).toEqual(readFileContent(getFixturePath('result-stylish.txt')));
-  expect(
-    genDiff(
-      getFixturePath('fileLeft.yml'),
-      getFixturePath('fileRight.yml'),
-      'plain'
-    )
-  ).toEqual(readFileContent(getFixturePath('result-plain.txt')));
-  expect(
-    genDiff(
-      getFixturePath('fileLeft.yml'),
-      getFixturePath('fileRight.yml'),
-      'json'
-    )
-  ).toEqual(readFileContent(getFixturePath('result.json')));
+describe.each([
+  ['fileLeft.yml', 'fileRight.yml', 'stylish', 'result-stylish.txt'],
+  ['fileLeft.yml', 'fileRight.yml', 'plain', 'result-plain.txt'],
+  ['fileLeft.yml', 'fileRight.yml', 'json', 'result.json'],
+])('YAML files comparison:', (fileLeft, fileRight, outputFormat, expected) => {
+  test(`${getFormatDescription(outputFormat)} output: `, () => {
+    expect(
+      genDiff(
+        getFixturePath(fileLeft),
+        getFixturePath(fileRight),
+        outputFormat,
+      ),
+    ).toEqual(readFileContent(getFixturePath(expected)));
+  });
 });
