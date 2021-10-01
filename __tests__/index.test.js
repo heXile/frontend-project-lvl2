99b51b28cc3f6f7676ddd6d2b1783acdcb49637a
hxl <xiberian@icloud.com>
2021-10-01 11:03:46 +0300

    Fixed tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,36 +7,22 @@ import { readFileContent } from '../src/utils.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const getFormatDescription = (format) => (format === 'plain' ? 'plain text' : format);
 
 describe.each([
-  ['fileLeft.json', 'fileRight.json', 'stylish', 'result-stylish.txt'],
-  ['fileLeft.json', 'fileRight.json', 'plain', 'result-plain.txt'],
-  ['fileLeft.json', 'fileRight.json', 'json', 'result.json'],
-])('JSON files comparison:', (fileLeft, fileRight, outputFormat, expected) => {
-  test(`${getFormatDescription(outputFormat)} output: `, () => {
+  ['json', 'stylish'],
+  ['json', 'plain'],
+  ['json', 'json'],
+  ['yml', 'stylish'],
+  ['yml', 'plain'],
+  ['yml', 'json'],
+])('Gendiff files comparison:', (extension, outputFormat) => {
+  test(`${extension} files comparison with ${outputFormat} output format: `, () => {
     expect(
       genDiff(
-        getFixturePath(fileLeft),
-        getFixturePath(fileRight),
+        getFixturePath(`fileLeft.${extension}`),
+        getFixturePath(`fileRight.${extension}`),
         outputFormat,
       ),
-    ).toEqual(readFileContent(getFixturePath(expected)));
-  });
-});
-
-describe.each([
-  ['fileLeft.yml', 'fileRight.yml', 'stylish', 'result-stylish.txt'],
-  ['fileLeft.yml', 'fileRight.yml', 'plain', 'result-plain.txt'],
-  ['fileLeft.yml', 'fileRight.yml', 'json', 'result.json'],
-])('YAML files comparison:', (fileLeft, fileRight, outputFormat, expected) => {
-  test(`${getFormatDescription(outputFormat)} output: `, () => {
-    expect(
-      genDiff(
-        getFixturePath(fileLeft),
-        getFixturePath(fileRight),
-        outputFormat,
-      ),
-    ).toEqual(readFileContent(getFixturePath(expected)));
+    ).toEqual(readFileContent(getFixturePath(`result-${outputFormat}.txt`)));
   });
 });
